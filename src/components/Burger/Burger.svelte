89f76bf560a0c0dd84b17907aa043b34bb138152
Joshua Nwosu <joshuanwosu078@gmail.com>
2021-03-24 21:43:35 +0100

    added click event to burger menu

--- src/components/Burger/Burger.svelte
+++ src/components/Burger/Burger.svelte
@@ -1,8 +1,12 @@
 <script>
     import "./Burger.css";
+    import { toggleNavbar } from "../../store/clickFunc.js";
 </script>
 
-<button class="burger-menu" aria-label="Burger Menu">
+<button
+    class="burger-menu"
+    aria-label="Burger Menu"
+    on:click={() => ($toggleNavbar = !$toggleNavbar)}>
     <span />
     <span />
     <span />
