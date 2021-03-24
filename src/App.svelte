48b68881cb72df3c0a4dbd6d6406eae7306ae811
Joshua Nwosu <joshuanwosu078@gmail.com>
2021-03-24 21:35:09 +0100

    changed theme color to color_three

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 	import Control from "./components/Control/Control.svelte";
 	import Navigation from "./components/Navigation/Navigation.svelte";
 
-	document.body.classList.add("color_one");
+	document.body.classList.add("color_three");
 </script>
 
 <div class="app">
