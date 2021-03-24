2f8643e836bd1f01a52a06ae6293cefd7fb4997e
Joshua Nwosu <joshuanwosu078@gmail.com>
2021-03-24 09:48:50 +0100

    init file

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
 	import "./App.css";
+	import { MoreVertical, MoreHorizontal } from "./components/icons";
 </script>
 
 <div class="app">
 	<main>
 		<h1>FlamePLayer</h1>
+		<MoreVertical />
+		<MoreHorizontal />
 	</main>
 </div>
